657a657068629f1a7cae313a220ec4156fe0d1e5
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-11-15 16:58:18 +0200

    Setup auth

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,6 @@ export { default as Home } from './Home'
 export { default as WhoWeAre } from './WhoWeAre'
 export { default as OurProduct } from './OurProduct'
 export { default as Contact } from './Contact'
+
+export { default as Login } from './auth/Login'
+export { default as Register } from './auth/Register'
