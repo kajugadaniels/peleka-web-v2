0202088f9ebc6c8ee50eea8eabce3d4da0711c84
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-11-15 17:54:42 +0200

    Design hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,66 @@ import React from 'react'
 
 const Hero = () => {
     return (
-        <div>Hero</div>
+        <div className="page-title-home2">
+            <div className="tf-container">
+                <div className="row items-center">
+                    <div className="col-lg-6">
+                        <div className="content">
+                            <div className="widget box-sub-tag wow fadeInUp" data-wow-delay="0s">
+                                <div className="sub-tag-icon">
+                                    <i className="icon-flash"></i>
+                                </div>
+                                <div className="sub-tag-title">
+                                    <p>The Leader in Online Courier Delivery</p>
+                                </div>
+                            </div>
+                            <h1 className="fw-7 wow fadeInUp" data-wow-delay="0.2s">
+                                Peleka: Kigali’s Quickest Rides & Deliveries
+                            </h1>
+                            <h6 className="wow fadeInUp" data-wow-delay="0.3s">
+                                With Peleka, your packages and personal transport needs are just a tap away. Experience swift services across Kigali, powered by motorbikes.
+                            </h6>
+                            <div className="bottom-btns">
+                                <a href="#" className="tf-btn wow fadeInUp" data-wow-delay="0.4s">
+                                    Learn More About Us
+                                    <i className="icon-arrow-top-right"></i>
+                                </a>
+                                <div className="widget-video wow fadeInUp" data-wow-delay="0.5s">
+                                    <a href="#" className="popup-youtube">
+                                        <i className="flaticon-play fs-18"></i>
+                                    </a>
+                                    <h6 className="mb-0">Watch Demo</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-6">
+                        <div className="image">
+                            <div className="image1 align-items-end">
+                                <img className="lazyload" data-src="https://upskillreact.vercel.app/images/page-title/page-title-home2-1.jpg" src="https://upskillreact.vercel.app/images/page-title/page-title-home2-1.jpg" alt="" />
+                                <div className="box box1">
+                                    <div className="icon">
+                                        <i className="flaticon-online-training"></i>
+                                    </div>
+                                    <h2 className="fw-7">35K</h2>
+                                    <p className="fz-15">Riders</p>
+                                </div>
+                            </div>
+                            <div className="image2">
+                                <div className="box box2">
+                                    <div className="icon">
+                                        <i className="flaticon-user-2"></i>
+                                    </div>
+                                    <h2 className="fw-7">500K</h2>
+                                    <p className="fz-15">Happy Customers</p>
+                                </div>
+                                <img className="lazyload" data-src="https://upskillreact.vercel.app/images/page-title/page-title-home2-2.jpg" src="https://upskillreact.vercel.app/images/page-title/page-title-home2-2.jpg" alt="" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
