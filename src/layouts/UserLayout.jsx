bad5c8ef363c4370b660a809e9f7be2ecc9a6da4
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-11-15 17:43:06 +0200

    Setup hero page

--- src/layouts/UserLayout.jsx
+++ src/layouts/UserLayout.jsx
@@ -11,9 +11,19 @@ const UserLayout = () => {
 
             <Header />
 
-            <Outlet />
+            <Hero />
 
-            <Footer />
+            <div class="main-content">
+                <Outlet />
+
+                <Footer />
+            </div>
+
+            <div className="progress-wrap active-progress">
+                <svg className="progress-circle svg-content" style={{ width: '100%', height: '100%' }} viewBox="-1 -1 102 102">
+                    <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" style={{ transition: 'stroke-dashoffset 10ms linear 0s', strokeDasharray: '307.919, 307.919', strokeDashoffset: '286.138' }}></path>
+                </svg>
+            </div>
         </div>
     )
 }
