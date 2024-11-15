1410cc8f9b64d31e1b40d8fe7ed4c040c9dc523d
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-11-15 16:57:05 +0200

    Setup user visit pages

---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Home } from './Home'
+export { default as WhoWeAre } from './WhoWeAre'
+export { default as OurProduct } from './OurProduct'
+export { default as Contact } from './Contact'
