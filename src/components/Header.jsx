40adbd63ef4faadc8005d9a4beb972f89f1681c9
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-11-15 17:22:09 +0200

    Design header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,90 @@
 import React from 'react'
+import { Logo } from '../assets/img'
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <header id="header_main" className="header style-2">
+            <div className="header-inner">
+                <div className="header-inner-wrap">
+                    <div className="header-left flex-grow">
+                        <a className="mobile-nav-toggler mobile-button d-lg-none flex" href="#menu"></a>
+                        <div id="site-logo">
+                            <a href="/" rel="home">
+                                <img id="logo-header" src={Logo} alt="" style={{ width: '80px' }} />
+                            </a>
+                        </div>
+                    </div>
+                    <div className="header-right">
+                        <nav className="main-menu">
+                            <ul className="navigation">
+                                <li className="current">
+                                    <a href="/">
+                                        Home
+                                    </a>
+                                </li>
+                                <li className="current">
+                                    <a href="/who-we-are">
+                                        Who We Are
+                                    </a>
+                                </li>
+                                <li className="current">
+                                    <a href="/our-product">
+                                        Our Product
+                                    </a>
+                                </li>
+                                <li className="current">
+                                    <a href="/contact-us">
+                                        Contact Us
+                                    </a>
+                                </li>
+                            </ul>
+                        </nav>
+                        <div className="header-btn">
+                            <div className="header-login">
+                                <a href="/login" className="tf-button-default header-text">
+                                    Log In
+                                </a>
+                            </div>
+                            <div className="header-register">
+                                <a href="/register" className="tf-button-default active header-text">
+                                    Sign Up
+                                </a>
+                            </div>
+                            <div className="header-join d-lg-none flex">
+                                <a href="/login" className="fs-15">Login</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <nav className="d-lg-none" id="menu">
+                <a className="close" aria-label="Close menu" href="#wrapper">
+                    <i className="flaticon-close-1"></i>
+                </a>
+                <ul>
+                    <li className="current">
+                        <a href="/">
+                            <span>Home</span>
+                        </a>
+                    </li>
+                    <li className="">
+                        <a href="/who-we-are">
+                            <span>Who We Are</span>
+                        </a>
+                    </li>
+                    <li className="">
+                        <a href="/our-product">
+                            <span>Our Products</span>
+                        </a>
+                    </li>
+                    <li className="">
+                        <a href="/contact-us">
+                            <span>Contact</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     )
 }
 
--- src/layouts/UserLayout.jsx
+++ src/layouts/UserLayout.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import { Footer, Header } from '../components/'
 
 const UserLayout = () => {
     return (
-        <div>
-            UserLayout
+        <div className='wrapper'>
+            <div className="tf-top-bar style-1 flex items-center justify-center">
+                <p>Intro price. Get UpSkill for Big Sale -95% off.</p>
+            </div>
+
+            <Header />
+
             <Outlet />
+
+            <Footer />
         </div>
     )
 }
