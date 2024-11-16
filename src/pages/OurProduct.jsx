bf9326ea722743634edfb5d62c0658639c2b4c0f
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-11-16 18:08:25 +0200

    New changes

--- src/pages/OurProduct.jsx
+++ src/pages/OurProduct.jsx
@@ -2,7 +2,33 @@ import React from 'react'
 
 const OurProduct = () => {
     return (
-        <div>OurProduct</div>
+        <>
+            <div className="page-title basic">
+                <div className="tf-container full">
+                    <div className="row">
+                        <div className="col-12">
+                            <div className="content text-center">
+                                <ul className="breadcrumbs flex items-center justify-center gap-10">
+                                    <li>
+                                        <a href="/" className="flex">
+                                            <i className="icon-home"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <i className="icon-arrow-right"></i>
+                                    </li>
+                                    <li>Who We Are</li>
+                                </ul>
+                                <h2 className="font-cardo fw-7">Who We Are</h2>
+                                <h6>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.
+                                </h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
