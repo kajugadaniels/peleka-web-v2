82a044ae21045c55e9f8e4656dc62327baa296ec
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-11-16 16:41:24 +0200

    Design delivery request details page

--- src/pages/deliveryRequests/GetDeliveryRequestDetails.jsx
+++ src/pages/deliveryRequests/GetDeliveryRequestDetails.jsx
@@ -2,7 +2,72 @@ import React from 'react'
 
 const GetDeliveryRequestDetails = () => {
     return (
-        <div>GetDeliveryRequestDetails</div>
+        <>
+            <section className="section-product-top shop-single">
+                <div className="section-image-zoom">
+                    <div className="tf-container">
+                        <div className="row">
+                            <div className="col-lg-4">
+                                <div className="">
+                                    <img className=" tf-image-zoom ls-is-cached lazyloaded" data-zoom="https://creativelayers.net/themes/upskill-html/images/shop/shop-7.png" data-src="https://creativelayers.net/themes/upskill-html/images/shop/shop-7.png" src="https://creativelayers.net/themes/upskill-html/images/shop/shop-7.png" alt="" />
+                                </div>
+                            </div>
+                            <div className="col-lg-8">
+                                <div className="tf-product-infor-wrap position-relative">
+                                    <div className="tf-zoom-main"></div>
+                                    <div className="shop-detail-content">
+                                        <h2 className="product-title font-cardo">
+                                            HTML And CSS: Design And Build Websites
+                                        </h2>
+                                        <p className="author">
+                                            By:
+                                            <a href="#">Carolyn Welborn</a>
+                                        </p>
+                                        <div className="tf-price">
+                                            <div className="price-on-sale">
+                                                $&nbsp;249.00
+                                            </div>
+                                            <div className="compare-at-price">
+                                                $&nbsp;449.00
+                                            </div>
+                                        </div>
+                                        <div className="description">
+                                            <p className="fs-15">
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                            </p>
+                                        </div>
+                                        <div className="product_meta">
+                                            <div className="item">
+                                                <p>Sku:</p>
+                                                <p>RTA-0058</p>
+                                            </div>
+                                            <div className="item">
+                                                <p>Category:</p>
+                                                <p><a href="#">designer</a>, <a href="#">books</a></p>
+                                            </div>
+                                            <div className="item">
+                                                <p>Tags:</p>
+                                                <p><a href="#">ui</a>, <a href="#">ux</a>, <a href="#">web</a></p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section className="section-product-bottom">
+                <div className="tf-container">
+                    <div className="row">
+                        <div className="col-lg-12">
+                            Map Here
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
     )
 }
 
