72e05e5af6d84b81d794f211f086bfae987e129d
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-11-18 16:19:51 +0200

    Design terms and conditions

--- src/pages/TermsAndCondition.jsx
+++ src/pages/TermsAndCondition.jsx
@@ -28,6 +28,57 @@ const TermsAndCondition = () => {
                     </div>
                 </div>
             </div>
+
+            <div className="main-content pt-0">
+                <section className="section-page-terms">
+                    <div className="tf-container">
+                        <div className="row">
+                            <div className="col-12">
+                                <div className="content">
+                                    <div className="basic-terms ">
+                                        <h2 className="text-22 fw-5 font-outfit wow fadeInUp" data-wow-delay="0s">
+                                            Basic Terms
+                                        </h2>
+                                        <p className="text-1 fs-15 wow fadeInUp" data-wow-delay="0s">
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                        </p>
+                                        <p className="text-2 fs-15 wow fadeInUp" data-wow-delay="0s">
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                        </p>
+                                    </div>
+                                    <div className="right-and-restriction">
+                                        <h2 className="text-22 fw-5 font-outfit wow fadeInUp" data-wow-delay="0s">
+                                            Rights & Restrictions
+                                        </h2>
+                                        <ul className="list-text">
+                                            <li className="item-text fs-15 wow fadeInUp" data-wow-delay="0s">
+                                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                            </li>
+                                            <li className="item-text fs-15 wow fadeInUp" data-wow-delay="0s">
+                                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                            </li>
+                                            <li className="item-text fs-15 wow fadeInUp" data-wow-delay="0s">
+                                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div className="acces-and-use">
+                                        <h2 className="text-22 fw-5 font-outfit wow fadeInUp" data-wow-delay="0s">
+                                            Access and Use of the Services
+                                        </h2>
+                                        <p className="text-1 fs-15 wow fadeInUp" data-wow-delay="0s">
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                        </p>
+                                        <p className="text-2 fs-15 wow fadeInUp" data-wow-delay="0s">
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
         </>
     )
 }
