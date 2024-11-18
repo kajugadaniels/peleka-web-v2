96d0e8d9e47f52f7ce5b38f3dbf5b46fa21091bd
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-11-18 15:44:53 +0200

    Setup help center pages

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { AddDeliveryRequest, GetDeliveryRequestDetails, GetDeliveryRequests, Home, Login, Contact, OurProduct, Register, WhoWeAre, Dashboard, Profile, Faqs } from './pages'
+import { AddDeliveryRequest, GetDeliveryRequestDetails, GetDeliveryRequests, Home, Login, Contact, OurProduct, Register, WhoWeAre, Dashboard, Profile, Faqs, HelpCenter } from './pages'
 import UserLayout from './layouts/UserLayout'
 import { Route, Routes } from 'react-router-dom'
 import ProfileLayout from './layouts/ProfileLayout'
@@ -13,6 +13,7 @@ const AppRoutes = () => {
                 <Route path="/our-product" element={<OurProduct />} />
                 <Route path="/contact-us" element={<Contact />} />
                 <Route path="/faqs" element={<Faqs />} />
+                <Route path="/help-center" element={<HelpCenter />} />
 
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ export { default as WhoWeAre } from './WhoWeAre'
 export { default as OurProduct } from './OurProduct'
 export { default as Contact } from './Contact'
 export { default as Faqs } from './Faqs'
+export { default as HelpCenter } from './HelpCenter'
 
 export { default as Login } from './auth/Login'
 export { default as Register } from './auth/Register'
