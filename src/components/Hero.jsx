19cd3be721e80f09bf546a76b164490edd5b74a8
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-12-02 14:55:42 +0200

    Redesign hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,65 +1,137 @@
 import React from 'react'
+import { Image1, Image2 } from '../assets/img'
 
 const Hero = () => {
     return (
-        <div className="page-title-home2">
-            <div className="tf-container">
-                <div className="row items-center">
-                    <div className="col-lg-6">
-                        <div className="content">
-                            <div className="widget box-sub-tag wow fadeInUp" data-wow-delay="0s">
-                                <div className="sub-tag-icon">
-                                    <i className="icon-flash"></i>
-                                </div>
-                                <div className="sub-tag-title">
-                                    <p>The Leader in Online Courier Delivery</p>
-                                </div>
-                            </div>
-                            <h1 className="fw-7 wow fadeInUp" data-wow-delay="0.2s">
-                                Peleka: Kigali’s Quickest Rides & Deliveries
-                            </h1>
-                            <h6 className="wow fadeInUp" data-wow-delay="0.3s">
-                                With Peleka, your packages and personal transport needs are just a tap away. Experience swift services across Kigali, powered by motorbikes.
-                            </h6>
-                            <div className="bottom-btns">
-                                <a href="#" className="tf-btn wow fadeInUp" data-wow-delay="0.4s">
-                                    Learn More About Us
-                                    <i className="icon-arrow-top-right"></i>
-                                </a>
-                                <div className="widget-video wow fadeInUp" data-wow-delay="0.5s">
-                                    <a href="#" className="popup-youtube">
-                                        <i className="flaticon-play fs-18"></i>
-                                    </a>
-                                    <h6 className="mb-0">Watch Demo</h6>
+        <div className="page-title-home9">
+            <div className="swiper-container slider-home9">
+                <div className="swiper-wrapper">
+                    <div className="swiper-slide" data-year="Start Now">
+                        <div className="image">
+                            <img src={Image2} alt="" />
+                        </div>
+                        <div className="tf-container">
+                            <div className="row">
+                                <div className="col-12">
+                                    <div className="content">
+                                        <div>
+                                            <div className="widget box-sub-tag fade-item fade-item-1 ">
+                                                <div className="sub-tag-icon">
+                                                    <i className="icon-flash"></i>
+                                                </div>
+                                                <div className="sub-tag-title">
+                                                    <p>The Leader in Online Courier Delivery</p>
+                                                </div>
+                                            </div>
+                                            <h1 className="fade-item fade-item-1 fw-7 ">
+                                                Peleka: Kigali’s <br />Quickest Rides & Deliveries
+                                            </h1>
+                                            <div className="bottom-btns fade-item fade-item-1">
+                                                <a href="#" className="tf-btn style-secondary">
+                                                    Learn More About Us
+                                                    <i className="icon-arrow-top-right"></i>
+                                                </a>
+                                            </div>
+                                        </div>
+                                        <div className="widget-video">
+                                            <a href="#" className="popup-youtube">
+                                                <i className="flaticon-play fs-18"></i>
+                                            </a>
+                                            <h6 className="mb-0">Watch Demo</h6>
+                                        </div>
+                                    </div>
+                                    <div className="bot-wrap"></div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div className="col-lg-6">
+                    <div className="swiper-slide" data-year="Fast Courier Service">
                         <div className="image">
-                            <div className="image1 align-items-end">
-                                <img className="lazyload" data-src="https://upskillreact.vercel.app/images/page-title/page-title-home2-1.jpg" src="https://upskillreact.vercel.app/images/page-title/page-title-home2-1.jpg" alt="" />
-                                <div className="box box1">
-                                    <div className="icon">
-                                        <i className="flaticon-online-training"></i>
+                            <img src="images/page-title/page-title-home91.jpg" alt="" />
+                        </div>
+                        <div className="tf-container">
+                            <div className="row">
+                                <div className="col-12">
+                                    <div className="content">
+                                        <div>
+                                            <div className="widget box-sub-tag fade-item fade-item-1">
+                                                <div className="sub-tag-icon">
+                                                    <i className="icon-flash"></i>
+                                                </div>
+                                                <div className="sub-tag-title">
+                                                    <p>The Leader in Online Courier Delivery</p>
+                                                </div>
+                                            </div>
+                                            <h1 className="fw-7 fade-item fade-item-2">
+                                                Peleka: Kigali’s <br />Quickest Rides & Deliveries
+                                            </h1>
+                                            <div className="bottom-btns fade-item fade-item-3">
+                                                <a href="#" className="tf-btn style-secondary">
+                                                    Learn More About Us
+                                                    <i className="icon-arrow-top-right"></i>
+                                                </a>
+                                            </div>
+                                        </div>
+                                        <div className="widget-video">
+                                            <a href="#" className="popup-youtube">
+                                                <i className="flaticon-play fs-18"></i>
+                                            </a>
+                                            <h6 className="mb-0">Watch Demo</h6>
+                                        </div>
                                     </div>
-                                    <h2 className="fw-7">35K</h2>
-                                    <p className="fz-15">Riders</p>
+                                    <div className="bot-wrap"></div>
                                 </div>
                             </div>
-                            <div className="image2">
-                                <div className="box box2">
-                                    <div className="icon">
-                                        <i className="flaticon-user-2"></i>
+                        </div>
+                    </div>
+                    <div className="swiper-slide" data-year="Rider Booking">
+                        <div className="image">
+                            <img src={Image1} alt="" />
+                        </div>
+                        <div className="tf-container">
+                            <div className="row">
+                                <div className="col-12">
+                                    <div className="content">
+                                        <div>
+                                            <div className="widget box-sub-tag fade-item fade-item-1">
+                                                <div className="sub-tag-icon">
+                                                    <i className="icon-flash"></i>
+                                                </div>
+                                                <div className="sub-tag-title">
+                                                    <p>The Leader in Online Courier Delivery</p>
+                                                </div>
+                                            </div>
+                                            <h1 className="fw-7 fade-item fade-item-2">
+                                                Peleka: Kigali’s <br />Quickest Rides & Deliveries
+                                            </h1>
+                                            <div className="bottom-btns fade-item fade-item-3">
+                                                <a href="/who-we-are" className="tf-btn style-secondary">
+                                                    Learn More About Us
+                                                    <i className="icon-arrow-top-right"></i>
+                                                </a>
+                                            </div>
+                                        </div>
+                                        <div className="widget-video">
+                                            <a href="#" className="popup-youtube">
+                                                <i className="flaticon-play fs-18"></i>
+                                            </a>
+                                            <h6 className="mb-0">Watch Demo</h6>
+                                        </div>
                                     </div>
-                                    <h2 className="fw-7">500K</h2>
-                                    <p className="fz-15">Happy Customers</p>
+                                    <div className="bot-wrap"></div>
                                 </div>
-                                <img className="lazyload" data-src="https://upskillreact.vercel.app/images/page-title/page-title-home2-2.jpg" src="https://upskillreact.vercel.app/images/page-title/page-title-home2-2.jpg" alt="" />
                             </div>
                         </div>
                     </div>
                 </div>
+                <div className="tf-container">
+                    <div className="row">
+                        <div className="col-12">
+                            <div className="swiper-pagination"></div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
     )
