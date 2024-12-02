e25c2cfa952a0e1d5125ac534a5a738d5b7f0d07
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-12-02 13:56:00 +0200

    New changes

--- src/layouts/UserLayout.jsx
+++ src/layouts/UserLayout.jsx
@@ -5,10 +5,6 @@ import { Footer, Header, Hero } from '../components/'
 const UserLayout = () => {
     return (
         <div className='wrapper'>
-            <div className="tf-top-bar style-1 flex items-center justify-center">
-                <p>Intro price. Get UpSkill for Big Sale -95% off.</p>
-            </div>
-
             <Header />
 
             <Outlet />
