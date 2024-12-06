fd239122201343683a568629f030238aef865812
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-12-06 20:14:26 +0200

    New changes

--- src/api.js
+++ src/api.js
@@ -9,6 +9,9 @@ const api = axios.create({
     baseURL: API_BASE_URL,
     headers: {
         'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Authorization, Content-Type',
     },
 });
 
